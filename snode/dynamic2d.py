d0c9a2b1304eaaca2556e36ffba3a27cad1496d3
mrzhuzhe <466648706@qq.com>
2022-10-21 12:18:33 +0800

    test snode  buffer overflow

--- snode/dynamic2d.py
+++ snode/dynamic2d.py
@@ -24,16 +24,22 @@ def make_lists():
                 _count += 1
             print("------", i, j, _count)
             l[i, j] = ti.length(x.parent(), (i, j))  
-            print(i, j, ti.length(x.parent(), (i, j)))
+            #print(i, j, ti.length(x.parent(), (i, j)))
 
 @ti.kernel
-def read_lists():
-    for i in range(n):
-        for j in range(n):
+def read_lists(d: int):
+    for i in range(n * d):
+        for j in range(n * d):
             print("***", i, j, l[i, j])
             for k in range(l[i, j]):
                 print("x[i,j,k]", i, j, k, ":", x[i, j, k])
 
 #ti.deactivate_all_snodes()  
 make_lists()
-read_lists()
+read_lists(d=1)
+ti.deactivate_all_snodes()
+print("read 2")
+read_lists(d=2)
+print("read 3")
+make_lists()
+read_lists(d=2)
