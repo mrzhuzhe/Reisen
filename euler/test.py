375892e870372d802a2c21d9cefa2d6958dca147
mrzhuzhe <466648706@qq.com>
2023-01-04 16:04:12 +0800

    it move

--- euler/test.py
+++ euler/test.py
@@ -1,4 +1,3 @@
-from matplotlib.pyplot import flag
 import taichi as ti
 import math
 
@@ -12,8 +11,8 @@ scale = c_w / sim_w
 
 #gravity = -9.81
 gravity = 0
-dt = 1.0 / 120.0
-numSteps = 100
+dt = 1.0 / 60.0
+numSteps = 40
 radius = 0.15
 res = 200
 density = 1000
@@ -105,18 +104,9 @@ def sampleFieldM(x, y , field: int):
 	h1 = 1 / h
 	x = ti.max(ti.min(x, numX * h), h)
 	y = ti.max(ti.min(y, numY * h), h)
-	dx = 0
-	dy = 0
-	if field == U_FIELD:
-		#f = U
-		dy = h2
-	elif field == V_FIELD:
-		#f = V
-		dx = h2
-	elif field == S_FIELD:
-		#f = M
-		dx = h2 
-		dy = h2 
+	
+	dx = h2 
+	dy = h2 
 
 	x0 = int(ti.min(ti.floor((x-dx)*h1), numX-1))
 	tx = ((x - dx) - x0*h) * h1
@@ -144,7 +134,7 @@ def sampleFieldM(x, y , field: int):
 		#print("is me???")
 		_vars = sx*sy * M[x0, y0] + tx*sy * M[x1, y0] + tx*ty * M[x1, y1] + sx*ty * M[x0, y1]
 		
-	return _vars 
+	return _vars # notice this is equal to 0
 	"""
 	return sx*sy * M[x0, y0] + tx*sy * M[x1, y0] + tx*ty * M[x1, y1] + sx*ty * M[x0, y1]
 	
@@ -170,17 +160,23 @@ def solveIncompressibility(substep: int, dt: float):
 		Sy0 = S[i, j-1]
 		Sy1 = S[i, j+1]
 		_s = Sx0 + Sx1 + Sy0 + Sy1
+		#print(_s)
 		if _s == 0:
 			continue
 		
 		_div = U[i+1, j] - U[i, j] + V[i, j+1] - V[i, j] 
 		_p = - _div / _s
+		#if _div != 0 :	print(_div)
+		#_p = _p * 1.9
+		#print(_p)
 		# todo sor here
 		P[i, j] += cp * _p
+
 		U[i, j] -= Sx0 * _p
 		U[i+1, j] += Sx1 * _p
 		V[i, j] -= Sy0 * _p
 		V[i, j+1] += Sy1 * _p
+		#print(i,j, U[i, j], U[i+1, j], V[i, j], V[i, j+1])
 
 @ti.kernel
 def extrapolate():
@@ -229,12 +225,16 @@ def advectSmoke(dt: float):
 
 	for i, j in ti.ndrange((1, numX-1), (1, numY-1)):
 		if S[i, j] != 0:
+			#if U[i, j] != 0: print(U[i, j])
+			#if V[i, j] != 0: print(V[i, j])
 			_u = (U[i, j] + U[i+1, j]) * 0.5
 			_v = (V[i, j] + V[i, j+1]) * 0.5
+			#print(_u, _v)	
 			x = i * h + h2 - dt * _u 
 			y = j * h + h2 - dt * _v 
 			#newM[i, j] = sampleField(x, y, S_FIELD)
 			newM[i, j] = sampleFieldM(x, y, S_FIELD)
+			#if newM[i, j] < 0.99: print(i, j, newM[i, j])
 
 	for i, j in ti.ndrange(numX, numY):
 		M[i, j] = newM[i, j]
@@ -251,12 +251,13 @@ def draw():
 		#cx = ti.floor(scale * h) + 1
 		#cy = ti.floor(scale * h) + 1
 		#p = 4 * (y * c_w + x)
-		pixels[int(x), int(y)] = 255*_s
+		#pixels[int(x), int(y)] = 255*_s
+		pixels[int(x), int(y)] = _s
 			
 
 @ti.kernel
 def init():
-	inVel = 2.0
+	inVel = 20.0
 	for i, j in ti.ndrange(numX, numY):
 		_s = 1.0	
 		if (i == 0 | j == 0 | j == numY-1):
@@ -280,18 +281,19 @@ def init():
     #pass
 
 def update():
-	integrate(dt, gravity)
+	#integrate(dt, gravity)
 	P.fill(0)
 	for substep in range(numSteps):
 		solveIncompressibility(substep, dt)
 
-	extrapolate()
-	advectVel(dt)
+	#extrapolate()
+	#advectVel(dt)
 	advectSmoke(dt)
 
 gui = ti.GUI('langrange-2D', (c_w, c_h))
 
 init()
+
 
 while gui.running:
 	update()
