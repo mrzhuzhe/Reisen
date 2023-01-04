7d610608f05440b27236d5cbbcbd5c14a1b560b3
mrzhuzhe <466648706@qq.com>
2023-01-04 17:44:28 +0800

    ?

--- euler/test.py
+++ euler/test.py
@@ -53,26 +53,16 @@ def avgU(i, j):
 
 @ti.func
 def avgV(i, j):
-	return (V[i-1, j] + U[i, j] + U[i-1, j+1] + U[i,j+1]) * 0.25
+	return (V[i-1, j] + V[i, j] + V[i-1, j+1] + V[i, j+1]) * 0.25
 
 @ti.func 
-def sampleField(x, y , field: int):
+def sampleFieldU(x, y , field: int):
 	h1 = 1 / h
 	x = ti.max(ti.min(x, numX * h), h)
 	y = ti.max(ti.min(y, numY * h), h)
-	dx = 0
-	dy = 0
 	
-	if field == U_FIELD:
-		#f = U
-		dy = h2
-	elif field == V_FIELD:
-		#f = V
-		dx = h2
-	elif field == S_FIELD:
-		#f = M
-		dx = h2 
-		dy = h2 
+	dx = 0.0
+	dy = h2 
 
 	x0 = int(ti.min(ti.floor((x-dx)*h1), numX-1))
 	tx = ((x - dx) - x0*h) * h1
@@ -82,22 +72,34 @@ def sampleField(x, y , field: int):
 	ty = ((y - dy)- y0*h)*h1
 	y1 = int(ti.min(y0+1, numY-1))
 
-	sx = 1 - tx 
-	sy = 1 - ty
+	sx = 1.0 - tx 
+	sy = 1.0 - ty
+
+	return sx*sy * U[x0, y0] + tx*sy * U[x1, y0] + tx*ty * U[x1, y1] + sx*ty * U[x0, y1]	
 	
-	#print(x0, x1, y0, y1)
+
+@ti.func 
+def sampleFieldV(x, y , field: int):
+	h1 = 1 / h
+	x = ti.max(ti.min(x, numX * h), h)
+	y = ti.max(ti.min(y, numY * h), h)
 	
-	_coeff = ti.Vector([sx*sy, tx*sy, tx*ty, sx*ty])
-	#print(sx*sy, tx*sy, tx*ty, sx*ty)
-	_vars = ti.Vector([0, 0 , 0, 0])
-	if field == U_FIELD:
-		_vars = ti.Vector([U[x0, y0], U[x1, y0], U[x1, y1], U[x0, y1]])		
-	elif field == V_FIELD:
-		_vars = ti.Vector([V[x0, y0], V[x1, y0], V[x1, y1], V[x0, y1]])		
-	elif field == S_FIELD:		
-		_vars = ti.Vector([M[x0, y0], M[x1, y0], M[x1, y1], M[x0, y1]])	
-		
-	return _coeff.dot(_vars)
+	dx = h2 
+	dy = 0.0
+
+	x0 = int(ti.min(ti.floor((x-dx)*h1), numX-1))
+	tx = ((x - dx) - x0*h) * h1
+	x1 = int(ti.min(x0 +1, numX-1))
+
+	y0 = int(ti.min(ti.floor((y-dy)*h1), numY-1))
+	ty = ((y - dy)- y0*h)*h1
+	y1 = int(ti.min(y0+1, numY-1))
+
+	sx = 1.0 - tx 
+	sy = 1.0 - ty
+
+	return sx*sy * V[x0, y0] + tx*sy * V[x1, y0] + tx*ty * V[x1, y1] + sx*ty * V[x0, y1]
+	
 
 @ti.func 
 def sampleFieldM(x, y , field: int):
@@ -119,23 +121,6 @@ def sampleFieldM(x, y , field: int):
 	sx = 1 - tx 
 	sy = 1 - ty
 
-
-	#print(S_FIELD == field)
-
-	"""
-	_vars = 0
-	if field == U_FIELD:
-		#print("is me U_FIELD ????")
-		_vars = sx*sy * U[x0, y0] + tx*sy * U[x1, y0] + tx*ty * U[x1, y1] + sx*ty * U[x0, y1]	
-	elif field == V_FIELD:
-		#print("is me V_FIELD ????")
-		_vars = sx*sy * V[x0, y0] + tx*sy * V[x1, y0] + tx*ty * V[x1, y1] + sx*ty * V[x0, y1]
-	elif field == S_FIELD:		
-		#print("is me???")
-		_vars = sx*sy * M[x0, y0] + tx*sy * M[x1, y0] + tx*ty * M[x1, y1] + sx*ty * M[x0, y1]
-		
-	return _vars # notice this is equal to 0
-	"""
 	return sx*sy * M[x0, y0] + tx*sy * M[x1, y0] + tx*ty * M[x1, y1] + sx*ty * M[x0, y1]
 	
 
@@ -195,24 +180,28 @@ def advectVel(dt: float):
 		newV[i, j] = V[i, j]
 
 	for i, j in ti.ndrange((1, numX), (1, numY)):
-		if S[i, j] != 0 and S[i-1, j] != 0 and j < numY -1 :
+		# u component
+		if S[i, j] != 0 and S[i-1, j] !=0 and j<numY-1:
 			x = i * h
 			y = j * h + h2
 			_u = U[i, j]
 			_v = avgV(i, j)
 			x = x - dt * _u
 			y = y - dt * _v
-			_u = sampleField(x, y, U_FIELD)
+			_u = sampleFieldU(x, y, U_FIELD)
 			newU[i, j] = _u
-		if S[i, j] != 0 and S[i, j-1] !=0 and i < numX -1:
+			#print(_u)
+		# v component
+		if S[i, j] != 0 and S[i, j-1] !=0 and i<numX-1:
 			x = i * h + h2
 			y = j * h
 			_u = avgU(i, j)
 			_v = V[i, j]
 			x = x - dt * _u
 			y = y - dt * _v 
-			_v = sampleField(x, y, V_FIELD)
+			_v = sampleFieldV(x, y, V_FIELD)
 			newV[i, j] = _v 
+			#print(_v)
 
 	for i, j in ti.ndrange(numX, numY):
 		U[i, j] = newU[i, j]
@@ -257,7 +246,7 @@ def draw():
 
 @ti.kernel
 def init():
-	inVel = 20.0
+	inVel = 2.0
 	for i, j in ti.ndrange(numX, numY):
 		_s = 1.0	
 		if (i == 0 | j == 0 | j == numY-1):
@@ -281,19 +270,21 @@ def init():
     #pass
 
 def update():
-	#integrate(dt, gravity)
+	integrate(dt, gravity)
 	P.fill(0)
 	for substep in range(numSteps):
 		solveIncompressibility(substep, dt)
 
-	#extrapolate()
-	#advectVel(dt)
+	extrapolate()
+	advectVel(dt)
 	advectSmoke(dt)
 
 gui = ti.GUI('langrange-2D', (c_w, c_h))
 
 init()
 
+#update()
+#draw()
 
 while gui.running:
 	update()
