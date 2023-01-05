7ed083d312bc11837147fc69b41816e6a6a8a9b0
mrzhuzhe <466648706@qq.com>
2023-01-05 20:30:48 +0800

    need another way

--- euler/pic.py
+++ euler/pic.py
@@ -1,10 +1,13 @@
-from pyparsing import col
+from xml.etree.ElementTree import PI
+from psutil import pid_exists
 import taichi as ti
 
-ti.init(arch="gpu")
+ti.init(arch=ti.gpu)
 
 c_width = 600
 c_height = 600
+width = 1
+height = 1
 
 FLUID_CELL = 0
 AIR_CELL = 1
@@ -25,10 +28,11 @@ dy = ti.sqrt(3.0) / 2.0 * dx
 numX = 100
 numY = 100
 
-fnumX = 1 / h + 1 
-fnumY = 1 / h + 1
+fnumX = int(1 / h + 1) 
+fnumY = int(1 / h + 1)
 fInvSpacing = 1 / h
 fNumCells = fnumX * fnumY
+
 
 U = ti.field(dtype=ti.f32, shape=(fnumX, fnumY))
 V = ti.field(dtype=ti.f32, shape=(fnumX, fnumY))
@@ -52,6 +56,26 @@ particleColor = ti.Vector.field(3, dtype=ti.f32, shape=numParticles)
 
 particleVel = ti.Vector.field(2, dtype=ti.f32, shape=numParticles)
 
+particleDensity = ti.field(dtype=ti.f32, shape=fNumCells)
+particleRestDensity = 0
+
+particleRadius = r
+pInvSpacing = 1.0 / (2.2 * particleRadius)
+
+print("pInvSpacing", pInvSpacing)
+
+
+pNumX = ti.floor(width * pInvSpacing) + 1
+pNumY = ti.floor(height * pInvSpacing) + 1
+
+pNumCells = pNumX * pNumY
+numCellParticles = ti.field(dtype=ti.f32, shape=pNumCells)
+firstCellParticle = ti.field(dtype=ti.f32, shape=(pNumCells+1))
+
+cellParticleIds =  ti.field(dtype=ti.f32, shape=numParticles)
+
+numParticles = 0
+print("pNumX pNumY", pNumX, pNumY)
 
 pixels = ti.field(dtype=ti.f32, shape=(c_width, c_height))
 
@@ -71,6 +95,77 @@ def init():
         particlePos[cnt][0] = h + r + dx * i + (0 if j % 2 == 0 else r)
         particlePos[cnt][1] = h + r + dy * j
         cnt +=1
+
+@ti.kernel
+def integrateParticles(dt: float, gravity: float):
+    for i in ti.ndrange(numParticles):
+        particleVel[i][1] += dt * gravity
+        particlePos[i] += particleVel[i] * dt        
+
+@ti.kernel
+def pushParticlesApart(numIters):
+    colorDiffusionCoeff = 0.001
+    
+    # count particles per cell
+    numCellParticles.fill(0)
+
+    for i in ti.ndrange(numParticles):
+        x, y = particlePos[i]
+
+        xi = clamp(ti.floor(x * pInvSpacing), 0, pNumX-1)
+        yi = clamp(ti.floor(y * pInvSpacing), 0, pNumY-1) 
+        numCellParticles[xi, yi] += 1
+
+    # partial sums 
+    first = 0
+    for i in ti.ndrange(pNumCells):
+        first += numCellParticles[i]
+        firstCellParticle[i] = first
+    firstCellParticle[pNumCells] = first
+
+    # fill particles into cells
+    for i in ti.ndrange(numParticles):
+        x, y = particlePos[i]
+        xi = clamp(ti.floor(x* pInvSpacing), 0, pNumX-1)
+        yi = clamp(ti.floor(y* pInvSpacing), 0, pNumY-1)
+        firstCellParticle[xi, yi] -= 1
+        cellParticleIds[firstCellParticle[xi, yi]] = i
+
+    minDist = 2 * particleRadius
+    minDist2 = minDist * minDist
+
+    for iter in range(numIters):
+        for i in ti.ndrange(numParticles):
+            px, py = particlePos[i]
+            pxi = ti.floor(px * pInvSpacing)
+            pyi = ti.floor(py * pInvSpacing)
+            x0 = ti.max(pxi-1, 0)
+            y0 = ti.max(pyi-1, 0)
+            x1 = ti.min(pxi+1, pNumX-1)
+            y1 = ti.min(pyi+1, pNumY-1)
+
+            for xi, yi in ti.ndrange((x0, x1), (y0, y1)):
+                first = firstCellParticle[xi, yi]
+                last = firstCellParticle[xi, yi+1]
+                for j in range(first, last):
+                    id = cellParticleIds[j]
+                    if id == i:
+                        continue
+                    qx, qy = particlePos[id]
+                    
+                    dx = qx - px 
+                    dy = qy - py
+                    d2 = dx * dx + dy * dy
+                    if (d2 > minDist2 or d2 == 0):
+                        continue
+                    d = ti.sqrt(d2)
+                    s = 0.5 * (minDist -d) / d
+                    dx *= S
+                    dy *= S
+                    particlePos[i] -= [dx, dy]
+                    particlePos[id] += [dx, dy]
+
+
 
 def update():
 	pass
