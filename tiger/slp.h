fd7fe05cdbda1e48aabfe0daf0310ef3d509c413
mrzhuzhe <466648706@qq.com>
2023-12-03 00:01:52 +0800

    tiger

--- tiger/slp.h
+++ tiger/slp.h
@@ -1,8 +1,26 @@
+#include "utils.h"
 
 typedef struct A_stm_ *A_stm;
 typedef struct A_exp_ *A_exp;
 typedef struct  A_expList_ *A_expList;
 typedef enum {A_plus, A_minus, A_time, A_div} A_binop;
 
+struct A_stm_ { 
+    enum {  A_compoundStm, A_assignStm, A_printStm } kind;
+    union {
+        struct {
+            A_stm stm1, stm2;            
+        } compound ;
+        struct  
+        {
+            string id;
+            A_exp exp;
+        } assgin;
+        struct  
+        {
+            A_expList exp;
+        } print;        
+    } u;
+};
 
-
+A_stm A_CompoundStm(A_stm stm1, A_stm stm2);
